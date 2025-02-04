4efdd63f1bd1d5e0a8366aa9f74f3b88e036377e
Nuutti <snuutti@protonmail.com>
2025-02-04 13:53:26 +0200

    Shooting line renderer

--- Assets/Shooting.cs
+++ Assets/Shooting.cs
@@ -5,9 +5,18 @@ public class Shooting : MonoBehaviour
     [SerializeField] private Transform spawnPoint;
     [SerializeField] private GameObject bulletPrefab;
     [SerializeField] private int spawnSpeed = 10;
+    [SerializeField] private LineRenderer lineRenderer;
+
+    private void Start()
+    {
+        lineRenderer.startWidth = 0.05f;
+        lineRenderer.endWidth = 0.05f;
+    }
     
     private void Update()
     {
+        UpdateLine();
+        
         if (!OVRInput.GetDown(OVRInput.Button.SecondaryIndexTrigger))
         {
             return;
@@ -16,5 +25,22 @@ public class Shooting : MonoBehaviour
         var obj = Instantiate(bulletPrefab, spawnPoint.position, spawnPoint.rotation);
         var rb = obj.GetComponent<Rigidbody>();
         rb.velocity = transform.forward * spawnSpeed;
+    }
+
+    private void UpdateLine()
+    {
+        lineRenderer.SetPosition(0, transform.position);
+        
+        var mask = LayerMask.GetMask("Target");
+        if (Physics.Raycast(transform.position, transform.forward, out var hit, 200f, mask))
+        {
+            lineRenderer.SetPosition(1, hit.point);
+            lineRenderer.material.color = Color.green;
+        }
+        else
+        {
+            lineRenderer.SetPosition(1, transform.position + transform.forward * 100);
+            lineRenderer.material.color = Color.red;
+        }
     }
 }
